e5e9754056dd0b1d937e065e2eb6e91eca4bee1f
huangxingke <huangxingke03@163.com>
2026-06-10 10:44:15 +0800

    ref ： 下载jira附件脚本完善支持忽略敏感大小写

--- script/update_download_jira.sh
+++ script/update_download_jira.sh
@@ -1,5 +1,15 @@
-sudo cp /home/huangxingke/project/Python/script/download_jira.sh /usr/local/bin/download_jira
-echo "更新--- /usr/local/bin/download_jira ----"
-sudo chmod +x /usr/local/bin/download_jira
-echo "更新--- /usr/local/bin/download_jira 成功----"
+#!/bin/bash
 
+set -e
+
+sudo rm -f /usr/local/bin/update_download_jira /usr/local/bin/download_jira
+
+sudo cp /home/huangxingke/project/Python/script/update_download_jira.sh /usr/local/bin/updateDownloadJira
+echo "更新--- /usr/local/bin/updateDownloadJira ----"
+sudo chmod +x /usr/local/bin/updateDownloadJira
+echo "更新--- /usr/local/bin/updateDownloadJira 成功----"
+
+sudo cp /home/huangxingke/project/Python/script/download_jira.sh /usr/local/bin/downloadJira
+echo "更新--- /usr/local/bin/downloadJira ----"
+sudo chmod +x /usr/local/bin/downloadJira
+echo "更新--- /usr/local/bin/downloadJira 成功----"
