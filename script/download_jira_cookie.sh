b7b5bb193d23065949fd23b910252c80233dd374
huangxingke <huangxingke03@163.com>
2026-06-26 14:34:17 +0800

    ref ：下载时COOKKIE报空优化

---
+++ script/download_jira_cookie.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# ==================== Cookie 配置（请保持最新） ====================
+COOKIE="JSESSIONID=B27DDFAFB3420DA9C0F0935849920D19; atlassian.xsrf.token=BWB8-5DWA-IKRE-Q10M_edd0a58de2bcec38f5febc0ff979a985db8b18b9_lin; jira.editor.user.mode=source"
